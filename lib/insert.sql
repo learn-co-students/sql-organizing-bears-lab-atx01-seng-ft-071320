82cae5a7552aca8f4f29f80e2d14b782e9e4ff2f
jtskidmore <johntoddskidmore@gmail.com>
2020-07-22 21:52:09 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,43 @@
+
+
+INSERT INTO
+bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 4, "M", "brown", "grumpy", 1);
+
+INSERT INTO
+bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 2, "M", "black", "playful", 1);
+
+INSERT INTO
+bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 7, "F", "brown", "friendly", 1);
+
+INSERT INTO
+bears (name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 10, "M", "brown", "reserved", 0);
+
+INSERT INTO
+bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 1, "F", "white", "shy", 1);
+
+INSERT INTO
+bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 150, "M", "green", "grumpy", 1);
+
+INSERT INTO
+bears (name, age, gender, color, temperament, alive)
+VALUES ("Wendy", 5, "F", "brown", "friendly", 1);
+
+INSERT INTO
+bears (age, gender, color, temperament, alive)
+VALUES (7, "M", "white", "mean", 1);
+
+-- * Mr. Chocolate
+-- * Rowdy
+-- * Tabitha
+-- * Sergeant Brown
+-- * Melissa
+-- * Grinch
+-- * Wendy
+-- * unnamed (the bear that killed Tim didn't have a name;
+--   refer back to how to create a record that doesn't have one value)
